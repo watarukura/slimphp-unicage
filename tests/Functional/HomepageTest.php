8ccd0ce85b661b5d47e2e2554ae3b5f559f5f29c
watarukura <what.r.j150@gmail.com>
2018-09-16 13:50:39 +0900

    phpunit error

--- tests/Functional/HomepageTest.php
+++ tests/Functional/HomepageTest.php
@@ -2,39 +2,28 @@
 
 namespace Tests\Functional;
 
-class HomepageTest extends BaseTestCase
+class HelloTest extends BaseTestCase
 {
-    /**
-     * Test that the index route returns a rendered response containing the text 'SlimFramework' but not a greeting
-     */
-    public function testGetHomepageWithoutName()
-    {
-        $response = $this->runApp('GET', '/');
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('SlimFramework', (string)$response->getBody());
-        $this->assertNotContains('Hello', (string)$response->getBody());
-    }
 
     /**
      * Test that the index route with optional name argument returns a rendered greeting
      */
-    public function testGetHomepageWithGreeting()
+    public function testGetHello()
     {
-        $response = $this->runApp('GET', '/name');
+        $response = $this->runApp('GET', '/Hello?User=Tanaka');
 
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('Hello name!', (string)$response->getBody());
+        $this->assertContains('Hello, Tanaka', (string)$response->getBody());
     }
 
     /**
      * Test that the index route won't accept a post request
      */
-    public function testPostHomepageNotAllowed()
+    public function testPostHello()
     {
-        $response = $this->runApp('POST', '/', ['test']);
+        $response = $this->runApp('POST', '/Hello', ['User' => 'Tanaka']);
 
-        $this->assertEquals(405, $response->getStatusCode());
-        $this->assertContains('Method not allowed', (string)$response->getBody());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('Hello, Tanaka', (string)$response->getBody());
     }
 }
